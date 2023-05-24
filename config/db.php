6a1a10c9ff6a0e090189bfc815d750bff237207a
Franz2610 <francescofroechlich1@gmail.com>
2023-05-24 20:54:51 +0200

    ok

---
+++ config/db.php
@@ -0,0 +1,47 @@
+<?php
+
+return
+$array_trains = [
+    [
+        'azienda' => 'Italo',
+        'stazione_di_partenza' => 'Roma Termini',
+        'stazione_di_arrivo' => 'Napoli Centrale',
+        'orario_di_partenza'  => '18:00:00' ,
+        'orario_di_arrivo' => '20:00:00' ,
+        'codice_treno' => '1254',
+        'numero_carrozze' => 4 ,
+        'in_orario' => 1 ,
+        'cancellato' => 0 ,
+    ],            [
+        'azienda' => 'Trenitalia',
+        'stazione_di_partenza' => 'Firenza',
+        'stazione_di_arrivo' => 'Afragola',
+        'orario_di_partenza'  => '11:00:00' ,
+        'orario_di_arrivo' => '16:00:00' ,
+        'codice_treno' => '4578',
+        'numero_carrozze' => 4 ,
+        'in_orario' => 1 ,
+        'cancellato' => 0 ,
+    ],            [
+        'azienda' => 'Regionale',
+        'stazione_di_partenza' => 'Napoli',
+        'stazione_di_arrivo' => 'Roma Tiburtina',
+        'orario_di_partenza'  => '10:00:00' ,
+        'orario_di_arrivo' => '14:00:00' ,
+        'codice_treno' => '2108',
+        'numero_carrozze' => 4 ,
+        'in_orario' => 1 ,
+        'cancellato' => 0 ,
+    ],            [
+        'azienda' => 'Italo',
+        'stazione_di_partenza' => 'Milano',
+        'stazione_di_arrivo' => 'Napoli',
+        'orario_di_partenza'  => '13:00:00' ,
+        'orario_di_arrivo' => '19:00:00' ,
+        'codice_treno' => 'abcdef',
+        'numero_carrozze' => 4 ,
+        'in_orario' => 1 ,
+        'cancellato' => 0 ,
+    ]
+
+  ];
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -15,54 +15,11 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        $array_trains = [
-            [
-                'azienda' => 'Italo',
-                'stazione_di_partenza' => 'Roma Termini',
-                'stazione_di_arrivo' => 'Napoli Centrale',
-                'orario_di_partenza'  => '18:00:00' ,
-                'orario_di_arrivo' => '20:00:00' ,
-                'codice_treno' => '1254',
-                'numero_carrozze' => 4 ,
-                'in_orario' => 1 ,
-                'cancellato' => 0 ,
-            ],            [
-                'azienda' => 'Trenitalia',
-                'stazione_di_partenza' => 'Firenza',
-                'stazione_di_arrivo' => 'Afragola',
-                'orario_di_partenza'  => '11:00:00' ,
-                'orario_di_arrivo' => '16:00:00' ,
-                'codice_treno' => '4578',
-                'numero_carrozze' => 4 ,
-                'in_orario' => 1 ,
-                'cancellato' => 0 ,
-            ],            [
-                'azienda' => 'Regionale',
-                'stazione_di_partenza' => 'Napoli',
-                'stazione_di_arrivo' => 'Roma Tiburtina',
-                'orario_di_partenza'  => '10:00:00' ,
-                'orario_di_arrivo' => '14:00:00' ,
-                'codice_treno' => '2108',
-                'numero_carrozze' => 4 ,
-                'in_orario' => 1 ,
-                'cancellato' => 0 ,
-            ],            [
-                'azienda' => 'Italo',
-                'stazione_di_partenza' => 'Milano',
-                'stazione_di_arrivo' => 'Napoli',
-                'orario_di_partenza'  => '13:00:00' ,
-                'orario_di_arrivo' => '19:00:00' ,
-                'codice_treno' => 'abcdef',
-                'numero_carrozze' => 4 ,
-                'in_orario' => 1 ,
-                'cancellato' => 0 ,
-            ]
 
-          ];
-        $array_trains = config('trains');
+       $array_trains = config('db', 'trains');
         foreach($array_trains as $train_item) {
                 $new_train_object = new Train();
-                $new_train_object->azienda = $train_item['azienza'];
+                $new_train_object->azienda = $train_item['azienda'];
                 $new_train_object->stazione_di_partenza = $train_item['stazione_di_partenza'];
                 $new_train_object->stazione_di_arrivo = $train_item['stazione_di_partenza'];
                 $new_train_object->orario_di_partenza = $train_item['orario_di_partenza'];
